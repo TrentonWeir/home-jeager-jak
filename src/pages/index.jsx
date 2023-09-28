bdb2de2e8e142792ef76b8d482f05082a74df93c
TJW <jaigerjakgames@gmail.com>
2023-09-27 20:38:36 -0400

    Updated Index Page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,59 @@
-export default function index() {
+import reactLogo from '../assets/react.svg'
+
+
+export default function Index() {
+    document.title = "Jeager Jak Domain"
+
+    let gitLogo = (size = '1em') => {
+        return (
+            <>
+                <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                </svg>
+            </>
+        )
+    }
     return (
         <>
-            <PageTitle>Jeager Jak Domain</PageTitle>
-
-            <h1>The default page for the jeager-jak.com domain</h1>
-
-            <content>
-                <section>
-                    <h2>Developer:</h2>
-                    <a href="https://github.com/TrentonWeir">Weirwolf Github</a>
-                </section>
+            <header>
                 <section>
                     <h2>Pages:</h2>
                     <ul>
+                        <li><a href="https://weir-outdoors.com">Weir Outdoors Default</a></li>
                         <li><a href="https://candels.weir-outdoors.com">Weir's Candles'</a></li>
                         <li><a href="https://jeffsarcherysupplies.weir-outdoors.com">Jeff's Archery Supplies</a></li>
                         <li><a href="https://games.jeager-jak.com">Jeager Jak Games</a></li>
+                        <li><a href="https://jeager-jak.com">Default Jeager Jak</a></li>
+                        <li><a href="https://books.jeager-jak.com">Anime / Book Review Site</a></li>
+
                     </ul>
+
                 </section>
-            </content></>
+                <section className='big-link-logos-section'>
+                    <div>
+                        <a href="https://vitejs.dev" target="_blank">
+                            <img src={'..\\vite.svg'} className="logo" alt="Vite logo" />
+                        </a>
+                        <a href="https://react.dev" target="_blank">
+                            <img src={reactLogo} className="logo react" alt="React logo" />
+                        </a>
+                        <a href='https://github.com/TrentonWeir' target='_blank'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-github logo" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                            </svg>
+                        </a>
+                    </div>
+                </section>
+            </header>
+
+            <section>
+                <main>
+                    <h1>This is a Default Landing page for <br /> the jeager-jak.com domain</h1>
+                    <h2>Developer:</h2>
+                    <a href="https://github.com/TrentonWeir">Weirwolf GitHub {gitLogo()}</a> <br />
+                    <a href="https://github.com/JeagerJakGames">Jeager Jak Games GitHub {gitLogo()}</a>
+                </main>
+            </section>
+        </>
     )
 }
